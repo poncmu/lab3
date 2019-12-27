6678a7b1970307635732fe1e328810716c910680
pondcmu <poncmu@gmail.com>
2019-12-27 16:48:25 +0700

    Pond 3-3 v2

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-	float i=2;
+	float i=6;
 	float sum=0;
-	while( i<=69)
+	while( i<=19)
 	{
 		sum=sum+(1/i);
 		i=i+1;
 	}
 	cout<<"Sum : "<<sum;
-    
+
 	return 0;
 }
